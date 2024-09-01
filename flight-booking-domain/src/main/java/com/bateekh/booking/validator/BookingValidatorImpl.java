5ef56c017ba44cc8360ceb86a4bacfd7f7c44b9e
Muhannad <mohannad.alrwajfa@network.global>
2024-09-01 21:19:06 +0300

    fix: handle missing validation

--- flight-booking-domain/src/main/java/com/bateekh/booking/validator/BookingValidatorImpl.java
+++ flight-booking-domain/src/main/java/com/bateekh/booking/validator/BookingValidatorImpl.java
@@ -1,14 +1,12 @@
 package com.bateekh.booking.validator;
 
-import com.bateekh.booking.dto.AvailableFlight;
-import com.bateekh.booking.dto.BookingDetails;
-import com.bateekh.booking.dto.Flight;
-import com.bateekh.booking.dto.SearchFlightsDetails;
+import com.bateekh.booking.dto.*;
 import com.bateekh.booking.exception.BookingException;
 import com.bateekh.booking.repository.BookingRepositoryAdaptor;
 import com.bateekh.booking.service.AvailableFlightsService;
 
 import java.util.List;
+import java.util.Objects;
 
 public class BookingValidatorImpl implements BookingValidator {
 
@@ -24,10 +22,11 @@ public class BookingValidatorImpl implements BookingValidator {
     @Override
     public void validate(BookingDetails bookingDetails) {
         Flight departureFlight = bookingRepositoryAdaptor.findByCode(bookingDetails.getDepartureFlightCode());
+        throwIfInvalidFlightCombination(bookingDetails, departureFlight);
+
         List<? extends AvailableFlight> availableFlights = availableFlightsService
                 .findAvailableFlights(toSearchFlightsDetails(bookingDetails, departureFlight));
-        throwIfNoAvailableFlights(availableFlights);
-        throwIfInvalidFlightCombination(bookingDetails, departureFlight);
+        throwIfNoSuitableFlights(availableFlights, bookingDetails);
     }
 
     private void throwIfInvalidFlightCombination(BookingDetails bookingDetails, Flight departureFlight) {
@@ -39,9 +38,23 @@ public class BookingValidatorImpl implements BookingValidator {
         }
     }
 
-    private void throwIfNoAvailableFlights(List<? extends AvailableFlight> availableFlights) {
-        if (availableFlights.isEmpty()) {
+    private void throwIfNoSuitableFlights(List<? extends AvailableFlight> availableFlights, BookingDetails bookingDetails) {
+        if (Objects.isNull(availableFlights) || availableFlights.isEmpty()) {
             throw new BookingException("No flights available for the provided booking details");
+        }
+        boolean isDepartureFlightAvailable = availableFlights.stream().anyMatch(availableFlight ->
+                availableFlight.getDepartureFlightDetails().getFlightCode().equals(bookingDetails.getDepartureFlightCode()));
+        if (!isDepartureFlightAvailable) {
+            throw new BookingException("No flights available for the provided booking details");
+        }
+        if (bookingDetails.isRoundTrip()) {
+            boolean isReturnFlightAvailable = availableFlights.stream().anyMatch(availableFlight -> {
+                AvailableRoundTripFlight availableRoundTripFlight = (AvailableRoundTripFlight) availableFlight;
+                return availableRoundTripFlight.getReturnFlightDetails().getFlightCode().equals(bookingDetails.getReturnFlightCode());
+            });
+            if (!isReturnFlightAvailable) {
+                throw new BookingException("No flights available for the provided booking details");
+            }
         }
     }
 
